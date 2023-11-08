e90cb4cf87cf536049b6335ab0e7a2f5c7edf672
Ahmed Borwin <56191075+Ahmedborwin@users.noreply.github.com>
2023-11-08 22:46:27 +0000

    add functionaility for multiple calls to stake stkae funciton

--- contracts/Stake.sol
+++ contracts/Stake.sol
@@ -9,7 +9,8 @@ contract Stake is ReentrancyGuard {
     error Stake__noMoniesStaked();
 
     uint256 public stakedTotal; // variable T
-    uint256 public pricePerToken; // variable S
+    uint256 public rewardPerToken; // variable S
+    uint256 constant TENTO18 = 1e18;
 
     //Events
     event Withdraw(address indexed staker, uint256 indexed amount); //withdraw event
@@ -20,7 +21,15 @@ contract Stake is ReentrancyGuard {
 
     // state variables
 
-    mapping(address => uint256) public stake;
+    struct StakerProfile {
+        address stakerAddress;
+        uint256 currentStake;
+        uint256 uncashedRewards;
+        uint256 rewardsPayedOut;
+        bool compoundRewards;
+    }
+
+    mapping(address => StakerProfile) public stakerToProfile;
     mapping(address => uint256) public mappingpricePerToken;
 
     ERC20 private usdT;
@@ -37,19 +46,30 @@ contract Stake is ReentrancyGuard {
     // fallback function (if exists)
 
     // external
-    function deposit() external payable {
+    function deposit(bool compoundRewards) external payable {
+        StakerProfile storage staker = stakerToProfile[msg.sender];
+        if (staker.stakerAddress == address(0)) {
+            staker.stakerAddress = msg.sender;
+        }
+        //check if already staked
+        if (staker.currentStake > 0) {
+            _reStake(staker, msg.value, compoundRewards); // pass staker address and value of new stake
+            //console.log("we hit restake");
+            return;
+        }
         //record amount staked
-        stake[msg.sender] += msg.value;
+        staker.currentStake += msg.value;
         //store reward per token at time of new stake
-        mappingpricePerToken[msg.sender] = pricePerToken;
-        //record total staked value => T
+        mappingpricePerToken[msg.sender] = rewardPerToken;
+        //record total staked value
         stakedTotal += msg.value;
         //emit event
         emit Stake(msg.sender, msg.value);
     }
 
     function withdraw() external nonReentrant {
-        _withdraw(msg.sender);
+        StakerProfile storage staker = stakerToProfile[msg.sender];
+        _withdraw(staker);
     }
 
     // public
@@ -58,25 +78,52 @@ contract Stake is ReentrancyGuard {
         if (stakedTotal == 0) {
             revert Stake__noMoniesStaked();
         } else {
-            pricePerToken = pricePerToken + (((_reward * 1e18) / stakedTotal));
+            rewardPerToken =
+                rewardPerToken +
+                (((_reward * TENTO18) / stakedTotal));
         }
     }
 
     // internal
-    function _withdraw(address _caller) internal {
-        uint256 deposited = stake[_caller];
+    function _withdraw(StakerProfile memory _staker) internal {
+        uint256 totalDeposited = _staker.currentStake;
 
-        uint256 reward = (deposited *
-            (pricePerToken - mappingpricePerToken[msg.sender])) / 1e18;
+        uint256 reward = (totalDeposited *
+            (rewardPerToken - mappingpricePerToken[msg.sender])) / TENTO18;
 
-        stakedTotal -= deposited;
-        stake[_caller] = 0;
-        uint256 depositAndReward = (deposited + reward);
-        //console.log(depositAndReward);
-        (bool sent, ) = _caller.call{value: depositAndReward}("");
+        stakedTotal -= totalDeposited; //offset amoutn withdrawn from stake total
+
+        _staker.currentStake = 0; //reset stake amount for user
+        _staker.rewardsPayedOut += reward; //record reward payed out to user
+
+        uint256 depositAndReward = (totalDeposited + reward);
+
+        (bool sent, ) = _staker.stakerAddress.call{value: depositAndReward}("");
         require(sent, "transfer failed");
-        emit Withdraw(_caller, depositAndReward);
+        emit Withdraw(_staker.stakerAddress, depositAndReward);
     }
 
+    function _reStake(
+        StakerProfile storage _staker,
+        uint256 _newDeposit,
+        bool compoundRewards
+    ) internal {
+        //calculate current reward
+        uint256 reward = (_staker.currentStake *
+            (rewardPerToken - mappingpricePerToken[msg.sender])) / TENTO18;
+        //record amount staked
+        _staker.currentStake += _newDeposit;
+        if (compoundRewards) {
+            _staker.currentStake += reward; //compound rewards by restaking
+        } else {
+            _staker.uncashedRewards += reward; //If compound rewards is false save amoutn of uncashed rewards
+        }
+        //store reward per token at time of new stake
+        mappingpricePerToken[msg.sender] = rewardPerToken;
+        //record total staked value => T
+        stakedTotal += msg.value;
+        //emit event
+        emit Stake(msg.sender, msg.value);
+    }
     // private
 }
--- test/Stake.js
+++ test/Stake.js
@@ -20,30 +20,32 @@ describe("Stake Contract", () => {
   });
   describe("Stake Function", async () => {
     it("updates mapping to reflect staked amount ", async () => {
-      await stake.connect(staker1).deposit({ value: ether(1) });
-      expect(await stake.stake(staker1.address)).equal(ether(1));
+      await stake.connect(staker1).deposit(false, { value: ether(1) });
+      const stakerProfile = await stake.stakerToProfile(staker1.address);
+      expect(stakerProfile.currentStake).equal(ether(1));
     });
-    it("updates price per token at time of stake event", async () => {
-      await stake.connect(staker1).deposit({ value: ether(1) });
+    it("stores price per token at time of stake event", async () => {
+      await stake.connect(staker1).deposit(false, { value: ether(1) });
       expect(
         (await stake.mappingpricePerToken(staker1.address)).toString()
-      ).equal("0");
+      ).equal("0"); //should equal to 0 on initial stake event
     });
-    it("updates total amount staked variable", async () => {
-      await stake.connect(staker1).deposit({ value: ether(1) });
+    it("updates total amount staked ", async () => {
+      await stake.connect(staker1).deposit(false, { value: ether(1) });
+
       expect(await stake.stakedTotal()).equal(ether(1));
     });
     it("stake event emitted", async () => {
-      expect(await stake.connect(staker1).deposit({ value: ether(1) }))
+      expect(await stake.connect(staker1).deposit(false, { value: ether(1) }))
         .emit(stake, "Stake")
         .withArgs(staker1.address, tokens(1));
     });
   });
   describe("Distribute Function", async () => {
     it("update price per token staked", async () => {
-      await stake.connect(staker1).deposit({ value: ether(1) });
+      await stake.connect(staker1).deposit(false, { value: ether(1) });
       await stake.distribute(tokens(10));
-      const pricePerToken = await stake.pricePerToken();
+      const pricePerToken = await stake.rewardPerToken();
     });
     it("rejects if no monies staked", async () => {
       await expect(
@@ -56,16 +58,11 @@ describe("Stake Contract", () => {
       beforeEach(
         ("stake set up",
         async () => {
-          await stake.connect(staker1).deposit({ value: ether(1) });
+          await stake.connect(staker1).deposit(false, { value: ether(1) });
           await stake.distribute(ether(10), { value: ether(10) });
         })
       );
       it("single staker to withdraw full reward , staked total updated and stake by address reset", async () => {
-        // console.log(
-        //   "Pre withdraw balance",
-        //   formatEth(await ethers.provider.getBalance(staker1.address))
-        // );
-
         await expect(stake.connect(staker1).withdraw()).changeEtherBalance(
           staker1.address,
           ether(11)
@@ -77,9 +74,9 @@ describe("Stake Contract", () => {
           .withArgs(staker1.address, ether(11));
       });
       it("proportional reward when multiple stakers", async () => {
-        await stake.connect(staker2).deposit({ value: ether(1) });
-        await stake.connect(staker3).deposit({ value: ether(1) });
-        await stake.connect(staker4).deposit({ value: ether(1) });
+        await stake.connect(staker2).deposit(false, { value: ether(1) });
+        await stake.connect(staker3).deposit(false, { value: ether(1) });
+        await stake.connect(staker4).deposit(false, { value: ether(1) });
         await stake.distribute(ether(10), { value: ether(10) });
         await expect(stake.connect(staker1).withdraw())
           .emit(stake, "Withdraw")
@@ -88,11 +85,40 @@ describe("Stake Contract", () => {
     });
     describe("failure", async () => {
       it("fails if transfer is unsuccesfull", async () => {
-        await stake.connect(staker1).deposit({ value: ether(1) });
+        await stake.connect(staker1).deposit(false, { value: ether(1) });
         await stake.distribute(ether(10));
         await expect(stake.connect(staker1).withdraw()).revertedWith(
           "transfer failed"
         );
+      });
+      it("", async () => {});
+      it("", async () => {});
+    });
+  });
+  describe("Restake Functionality", async () => {
+    describe("success", async () => {
+      beforeEach("stake ether", async () => {
+        await stake.connect(staker1).deposit(false, { value: ether(1) });
+        await stake.distribute(ether(10), { value: ether(10) });
+      });
+
+      it("Restake - Does not compound rewards:", async () => {
+        await stake.connect(staker1).deposit(false, { value: ether(1) });
+        const userProfile = await stake.stakerToProfile(staker1.address);
+        expect(userProfile.currentStake.toString()).equal(ether(2));
+        expect(userProfile.uncashedRewards.toString()).equal(ether(10));
+      });
+      it("Restake - Does compound rewards:", async () => {
+        await stake.connect(staker1).deposit(true, { value: ether(1) });
+        const userProfile = await stake.stakerToProfile(staker1.address);
+        expect(userProfile.currentStake.toString()).equal(ether(12));
+      });
+    });
+
+    describe("Unsuccessful", async () => {
+      beforeEach("stake ether", async () => {
+        await stake.connect(staker1).deposit(false, { value: ether(1) });
+        await stake.distribute(ether(10), { value: ether(10) });
       });
       it("", async () => {});
       it("", async () => {});
